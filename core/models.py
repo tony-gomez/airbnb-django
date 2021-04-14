be3d69812c939d215e337153948fead61399eaa1
tonyvives <vivestony1@gmail.com>
2021-04-14 11:04:15 -0500

    Formatting

--- core/models.py
+++ core/models.py
@@ -5,8 +5,8 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
 
-    #auto_now_add = django saves the time when is first created
-    #auto_now = django saves the time everytime the model is saved.
+    # auto_now_add = django saves the time when is first created
+    # auto_now = django saves the time everytime the model is saved.
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
